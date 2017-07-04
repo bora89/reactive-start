0a5fb9ed6d782ee3d29954528698fb33b13cd7cc
Alexander Borisenko <wealthybora@gmail.com>
2017-07-04 17:22:30 +0300

    added material-ui feature

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react'
 import {render} from  'react-dom';
 import Main from './modules/main/main';
 import './style.scss';
+// material-ui
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// Needed for onTouchTap
+// This dependency is temporary and will eventually go away
+injectTapEventPlugin();
 
 class Layout extends React.Component {
   render() {
     return (
-      <Main />
+      <MuiThemeProvider>
+        <Main />
+      </MuiThemeProvider>
     )
   }
 }
--- src/modules/main/main.js
+++ src/modules/main/main.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import './main.scss';
 
-export default class Main extends React.Component {
-    onClick () {
-        console.log('This is your call-to-action, take it seriously');
-    }
+import RaisedButton from 'material-ui/RaisedButton';
 
-    render () {
-        return (
-            <div class="main-tab">
-                <img class="app-logo" src={require('./logo.svg')} alt="app logo"/>
-                <p>
-                    This is the App demo
-                    <br/>
-                    Please add a short description of your App
-                </p>
-                <div class="main-bottom">
-                  <hr class="divider-long"/>
-                  <p>
-                    ReactiveStart 2017
-                  </p>
-                </div>
-            </div>
-        )
-    }
+export default class Main extends React.Component {
+  onClick() {
+    console.log('This is your call-to-action, take it seriously');
+  }
+
+  render() {
+    return (
+      <div class="main-tab">
+        <img class="app-logo" src={require('./logo.svg')} alt="app logo"/>
+        <p>
+          This is the App demo
+          <br/>
+          Please add a short description of your App
+        </p>
+
+        <RaisedButton label="Cool Button" primary={true} onTouchTap={ (obj) => {
+          alert('You just clicked the button!');
+        }}/>
+
+        <div class="main-bottom">
+          <hr class="divider-long"/>
+          <p>
+            ReactiveStart 2017
+          </p>
+        </div>
+      </div>
+    )
+  }
 }
